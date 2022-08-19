084ea69fd0f43ca3dc414e95041076fb63f02865
Leoasa1 <leoasa84@gmail.com>
2022-08-18 17:12:13 -0700

    Updated post request

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -4,6 +4,7 @@ const { roomModel } = require('../models/Room');
 const { bookingModel } = require('../models/Booking');
 const { guestModel } = require('../models/Guest');
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 // route    GET
 // descr    Get Booking
@@ -21,73 +22,90 @@ router.get('/', auth, async (req, res) => {
 	}
 });
 
-
 // route    POST
 // descr    Create Booking
 // access   public
-router.post('/', async (req, res) => {
-	const {
-		first_name,
-		last_name,
-		email,
-		phone,
-		room_type,
-		bed,
-		check_in,
-		check_out,
-	} = req.body;
-
-	if (!check_in || !check_out) {
-		throw new Error('Valid checkin, checkout dates are required.');
-	}
-
-	try {
-		const bookings = await bookingModel
-			.find({
-				check_in: {
-					$gte: check_in,
-				},
-				check_out: {
-					$lte: check_out,
-				},
-			})
-			.exec();
-
-		const room = await roomModel.findOne({
-			_id: {
-				$nin: !!bookings ? bookings.map((booking) => booking.room) : [],
-			},
-			room_type: {
-				$in: room_type,
-			},
-			bed: {
-				$in: bed,
-			},
-		});
-
-		let guest = await guestModel.findOne({ email });
-
-		if (!guest) {
-			guest = await guestModel.create({
-				first_name,
-				last_name,
-				email,
-				phone,
-			});
-		}
-
-		booking = await bookingModel.create({
-			room,
-			guest,
+router.post(
+	'/',
+	[
+		check('first_name', 'Name is required').not().isEmpty(),
+		check('last_name', 'Lastname is required').not().isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('phone', 'Phone is required').not().isEmpty(),
+		check('check_in', 'check_in is required').not().isEmpty(),
+		check('check_out', 'check_out is required').not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		const {
+			first_name,
+			last_name,
+			email,
+			phone,
+			room_type,
+			bed,
 			check_in,
 			check_out,
-		});
+		} = req.body;
 
-		res.status(200).send('Booking Created');
-	} catch (err) {
-		console.log(err.message);
-		res.status(500).send('Server error');
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		if (!check_in || !check_out) {
+			res.status(500).send('Server error');
+		}
+
+		try {
+			const bookings = await bookingModel
+				.find({
+					check_in: {
+						$gte: check_in,
+					},
+					check_out: {
+						$lte: check_out,
+					},
+				})
+				.exec();
+
+			const room = await roomModel.findOne({
+				_id: {
+					$nin: !!bookings
+						? bookings.map((booking) => booking.room)
+						: [],
+				},
+				room_type: {
+					$in: room_type,
+				},
+				bed: {
+					$in: bed,
+				},
+			});
+
+			let guest = await guestModel.findOne({ email });
+
+			if (!guest) {
+				guest = await guestModel.create({
+					first_name,
+					last_name,
+					email,
+					phone,
+				});
+			}
+
+			booking = await bookingModel.create({
+				room,
+				guest,
+				check_in,
+				check_out,
+			});
+
+			res.status(200).send('Booking Created');
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send('Server error');
+		}
 	}
-});
+);
 
 module.exports = router;
